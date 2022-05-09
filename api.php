3de3da560640b66741addea231859ed8bb4fd5aa
akhil.s <akhil.soman@teamta.in>
2022-05-09 18:57:43 +0530

    Sprint 1 Completed

--- api.php
+++ api.php
@@ -12,6 +12,7 @@ License: GPLv2 or later
 Text Domain: api
 */
 
+
     function generate_public_key( $user_email = '' ) {
         $auth_key = defined( 'AUTH_KEY' ) ? AUTH_KEY : '';
         $public   = hash( 'md5', $user_email . $auth_key . date( 'U' ) );
@@ -19,66 +20,10 @@ Text Domain: api
     }
 
     
-    function generate_private_key( $user_id = 0 ) {
-        $auth_key = defined( 'AUTH_KEY' ) ? AUTH_KEY : '';
-        $secret   = hash( 'md5', $user_id . $auth_key . date( 'U' ) );
-        return $secret;
-    }
+   
 
    function validate_token( $output = true ) {
         // Check that we're trying to authenticate
-        if ( ! isset( $_SERVER['PHP_AUTH_USER'] ) ) {
-            return $user;
-        }
-
-        $public_key = $_SERVER['PHP_AUTH_USER'];
-        $token      = $_SERVER['PHP_AUTH_PW'];
-
-        if ( empty( $public_key ) ) {
-            return new WP_Error(
-                'token_auth_no_key_to_validate',
-                __('Public key not sent.', 'wp-api-token-auth'),
-                array(
-                    'status' => 403,
-                )
-            );
-        }
-
-        if ( empty( $token ) ) {
-            return new WP_Error(
-                'token_auth_no_token_to_validate',
-                __('Token not sent.', 'wp-api-token-auth'),
-                array(
-                    'status' => 403,
-                )
-            );
-        }
-
-        if ( ! ( $user = $this->get_user( $public_key ) ) ) {
-            return new WP_Error(
-                'token_auth_invalid_public_key_validated',
-                __('Your request could not be authenticated. Invalid public key.', 'wp-api-token-auth'),
-                array(
-                    'status' => 403,
-                )
-            );
-        } else {
-            $token  = $token;
-            $secret = $this->get_user_secret_key( $user );
-            $public = $public_key;
-
-            if ( hash_equals( md5( $secret . $public ), $token ) ) {
-                return $user;
-            } else {
-                return new WP_Error(
-                    'token_auth_invalid_auth_validated',
-                    __('Your request could not be authenticated.', 'wp-api-token-auth'),
-                    array(
-                        'status' => 403,
-                    )
-                );
-            }
-        }
     }
 
 
@@ -88,9 +33,9 @@ add_action( 'rest_api_init', 'localphysio_authn_endpoint' );
 function localphysio_authn_endpoint(){
     register_rest_route(
         'api/v1/', // Namespace
-        'authn', // Endpoint
+        'login', // Endpoint
         array(
-            'methods'  => 'POST',
+            'methods'  => 'GET',
             'callback' => 'localphysio_callback_authn'
         )
     );
@@ -115,11 +60,15 @@ function localphysio_callback_authn( $request_data ) {
             
             if ( $wp_check_password_result ) {
                
-                $new_public_key = generate_public_key( $userdata->user_email );
-                $new_secret_key = generate_private_key( $userdata->ID );
+                $new_public_key = get_user_meta( $userdata->ID, 'auth_key' , true );
+                if($new_public_key == '')
+                {
+                        $new_public_key = generate_public_key($email);
+                        update_user_meta( $userdata->ID, 'auth_key', $new_public_key );
 
-                update_user_meta( $userdata->ID, 'rest_api_token_auth_public_key', $new_public_key );
-                update_user_meta( $userdata->ID, 'rest_api_token_auth_secret_key', $new_secret_key );
+                }
+
+  
                
 
 
@@ -128,10 +77,9 @@ function localphysio_callback_authn( $request_data ) {
             
                 $data['received_data'] = array(
                     'name'     => $email,
-                    //'password' => $password,
                     'data'     => $userdata,
-                    'public_key' => $new_public_key,
-                    'secret_key' => $new_secret_key 
+                    'auth_key' => $new_public_key,
+                    // 'secret_key' => $new_secret_key 
 
                 );
                 
@@ -174,7 +122,7 @@ add_action( 'rest_api_init', 'localphysio_signin_endpoint' );
 function localphysio_signin_endpoint(){
     register_rest_route(
         'api/v1/', // Namespace
-        'signin', // Endpoint
+        'signup', // Endpoint
         array(
             'methods'  => 'POST',
             'callback' => 'localphysio_callback_signin'
@@ -184,18 +132,10 @@ function localphysio_signin_endpoint(){
 
 function localphysio_callback_signin( $request_data ) {
 
-    // $key  = $request_data->get_header('Key');
-
-     // $public_key = $_SERVER['PHP_AUTH_USER'];
-     // $token      = $_SERVER['PHP_AUTH_PW'];
-
-       
-     
-    // $user_id = $this->validate_token( false );
+ 
 
      $data = array();
-     // $data['pub'] = $public_key;
-     // $data['token'] =   $token;
+
     
     $parameters = $request_data->get_params();
     $title     = $parameters['title'];
@@ -209,7 +149,10 @@ function localphysio_callback_signin( $request_data ) {
     $role = $parameters['role']; // staff_type
     $sec_key = $parameters['sec_key'];
     $btn_submit = $parameters['btn_submit'];
-
+    $domain_flag = $parameters['domain_flag'];
+    
+    if(is_email($email)) {
+    
     if(is_bool($btn_submit) == 1)
     {
 
@@ -235,19 +178,21 @@ function localphysio_callback_signin( $request_data ) {
     if(!is_wp_error($user_login)) {
     
     $auth_key = generate_public_key($email);
-    $token = generate_private_key($user_id);
+    //$token = generate_private_key($user_id);
 
     add_user_meta( $user_id, 'first_name', $firstname);
     add_user_meta( $user_id, 'last_name', $lastname);
     add_user_meta( $user_id, 'nickname', $firstname);
     add_user_meta( $user_id, 'staff_type', $role);
     add_user_meta( $user_id, 'auth_key', $auth_key);
-    add_user_meta( $user_id, 'token', $token);
+    add_user_meta( $user_id, 'domain_flag', $domain_flag);
+    
 
     $data['status'] = 'OK';
     $data['message'] = 'User created successfully!';
+    $data['uid'] = $user_id;
     $data['auth_key'] = $auth_key;
-    $data['token'] = $token;
+   // $data['token'] = $token;
     $response = new WP_REST_Response($data, 201);
        }
        else
@@ -291,6 +236,14 @@ else
     $response = new WP_REST_Response($data, 400);
 }
 
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Email id is not valid';
+    $response = new WP_REST_Response($data, 400);
+}
+
     return $response;
     }
 
@@ -304,7 +257,7 @@ add_action( 'rest_api_init', 'localphysio_insert_clinic_endpoint' );
 function localphysio_insert_clinic_endpoint(){
     register_rest_route(
         'api/v1/', // Namespace
-        'authn/2', // Endpoint
+        'signup/2', // Endpoint
         array(
             'methods'  => 'POST',
             'callback' => 'localphysio_callback_insert_clinic'
@@ -316,19 +269,24 @@ function localphysio_insert_clinic_endpoint(){
 function localphysio_callback_insert_clinic( $request_data ) {
 
     $data = array();
+    $key  = $request_data->get_header('Auth-key');
     $parameters = $request_data->get_params();
     $provideruid = $parameters['provideruid'];
     $uid = $parameters['uid'];
     $listings = $parameters['listings'];
-
-
     $btn_submit = $parameters['btn_submit'];
 
-    
-    if(is_bool($btn_submit) == 1)
+    $permission = get_user_meta( $uid, 'auth_key' , true );
+
+    if($key == $permission)
+    {
+             if(is_bool($btn_submit) == 1)
     {
 
     if($btn_submit == true)
+    {
+
+    if(!get_user_meta( $uid, 'title'))
     {
 
     foreach($listings as $key => $value) {
@@ -345,7 +303,7 @@ function localphysio_callback_insert_clinic( $request_data ) {
 
     $specialization_data = implode(",", $specialization );
     $treatments_data = implode(",", $treatments );
-    add_user_meta( $uid, 'provideruid', $provideruid);
+    add_user_meta( $uid, 'title',  $title);
     add_user_meta( $uid, 'address_1',  $address_1);
     add_user_meta( $uid, 'address_2', $address_2);
     add_user_meta( $uid, 'location', $location);
@@ -361,6 +319,16 @@ function localphysio_callback_insert_clinic( $request_data ) {
     $data['message'] = 'Clinic Added successfully!';
     $response = new WP_REST_Response($data, 201);
 
+
+    }
+    else
+    {
+    $data['status'] = 'Failed';
+    $data['message'] = 'Clinic exist in this account';
+    $response = new WP_REST_Response($data, 400);
+    }
+
+
     }
 else
 {
@@ -375,8 +343,223 @@ else
     $data['message'] = 'Parameters type error';
     $response = new WP_REST_Response($data, 400);
 }
+    }
+    else
+    {
+    $data['status'] = 'Failed';
+    $data['message'] = 'Authentication error';
+    $response = new WP_REST_Response($data, 400);
+    }
+
+    
+ 
+
 
     return  $response;
 
 }
 //------------Add clinic-end-------------//
+
+
+//------------Add payment start-------------//
+
+add_action( 'rest_api_init', 'localphysio_insert_payment_endpoint' );
+
+function localphysio_insert_payment_endpoint(){
+    register_rest_route(
+        'api/v1/', // Namespace
+        'signup/3', // Endpoint
+        array(
+            'methods'  => 'POST',
+            'callback' => 'localphysio_callback_insert_payment'
+        )
+    );
+}
+
+
+function localphysio_callback_insert_payment( $request_data ) {
+
+    $data = array();
+    $parameters = $request_data->get_params();
+    $key  = $request_data->get_header('Auth-key');
+    $provideruid = $parameters['provideruid'];
+    $uid = $parameters['uid'];
+    $paymentData = $parameters['paymentData'];
+
+    $permission = get_user_meta( $uid, 'auth_key' , true );
+
+    if($key == $permission)
+    {
+
+    foreach($paymentData as $key => $value) {
+     $creditor = $value['creditor'];
+     $mandate = $value['mandate'];
+     $customer = $value['customer'];
+     $customer_bank_account = $value['customer_bank_account'];
+     }
+   if(!get_user_meta( $uid, 'creditor'))
+    {
+    add_user_meta( $uid, 'creditor',  $creditor);
+    add_user_meta( $uid, 'mandate', $mandate);
+    add_user_meta( $uid, 'customer', $customer);
+    add_user_meta( $uid, 'customer_bank_account', $customer_bank_account);
+
+    $data['status'] = 'OK';
+    $data['message'] = 'Payment details added successfully!';
+    $response = new WP_REST_Response($data, 201);
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Payment exists in this account';
+    $response = new WP_REST_Response($data, 400);
+}
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Authentication error';
+    $response = new WP_REST_Response($data, 400);
+}
+
+    return  $response;
+}
+
+//------------Add payment end-------------//
+
+//------------Add clinic timing start-------------//
+
+add_action( 'rest_api_init', 'localphysio_insert_clinic_timing_endpoint' );
+
+function localphysio_insert_clinic_timing_endpoint(){
+    register_rest_route(
+        'api/v1/', // Namespace
+        'signup/clinic-details', // Endpoint
+        array(
+            'methods'  => 'POST',
+            'callback' => 'localphysio_callback_insert_clinic_timing'
+        )
+    );
+}
+
+
+function localphysio_callback_insert_clinic_timing( $request_data ) {
+   
+    $data = array();
+    $parameters = $request_data->get_params();
+    $key  = $request_data->get_header('Auth-key');
+    $provideruid = $parameters['provideruid'];
+    $uid = $parameters['uid'];
+    $initialAssessment = $parameters['initialAssessment'];
+    $followUpTreatment = $parameters['followUpTreatment'];
+    $openingTimes = $parameters['openingTimes']; //array
+    $parkingMode = $parameters['parkingMode'];
+    $paymentMode = $parameters['paymentMode'];
+    $insurancesAccepted = $parameters['insurancesAccepted']; //array
+    $clinicDescription = $parameters['clinicDescription'];
+    $logo = $parameters['logo'];
+
+    $permission = get_user_meta( $uid, 'auth_key' , true );
+
+    if($key == $permission)
+    {
+     
+      if(!get_user_meta( $uid, 'openingTimes'))
+    {
+     add_user_meta( $uid, 'initialAssessment',  $initialAssessment);
+     add_user_meta( $uid, 'followUpTreatment',  $followUpTreatment);
+     add_user_meta( $uid, 'parkingMode',  $parkingMode);
+     add_user_meta( $uid, 'paymentMode',  $paymentMode);
+     add_user_meta( $uid, 'clinicDescription',  $clinicDescription);
+     add_user_meta( $uid, 'logo',  $logo);
+
+    add_user_meta( $uid, 'openingTimes',  $openingTimes);
+    add_user_meta( $uid, 'insurancesAccepted', $insurancesAccepted);
+
+    $data['status'] = 'OK';
+    $data['message'] = 'Clinic timing added successfully!';
+    $response = new WP_REST_Response($data, 201);
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Clinic timing exists in this account';
+    $response = new WP_REST_Response($data, 400);
+}
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Authentication error';
+    $response = new WP_REST_Response($data, 400);
+}
+
+    return  $response;
+}
+//------------Add clinic timing end-------------//
+
+
+//------------Add practitioner-details start-------------//
+
+add_action( 'rest_api_init', 'localphysio_insert_practitioner_details_endpoint' );
+
+function localphysio_insert_practitioner_details_endpoint(){
+    register_rest_route(
+        'api/v1/', // Namespace
+        'signup/practitioner-details', // Endpoint
+        array(
+            'methods'  => 'POST',
+            'callback' => 'localphysio_callback_insert_practitioner_details'
+        )
+    );
+}
+
+
+function localphysio_callback_insert_practitioner_details( $request_data ) {
+
+    $data = array();
+    $parameters = $request_data->get_params();
+    $key  = $request_data->get_header('Auth-key');
+    $provideruid = $parameters['provideruid'];
+    $uid = $parameters['uid'];
+    $firstName = $parameters['firstName'];
+    $lastName = $parameters['lastName'];
+    $profileImage = $parameters['profileImage'];
+    $HCPC = $parameters['HCPC'];
+    $CSP = $parameters['CSP'];
+    $specialization = implode(",", $parameters['specialization'] ); //array
+    $description = $parameters['description'];
+
+    $permission = get_user_meta( $uid, 'auth_key' , true );
+    if($key == $permission)
+    {
+        if(!get_user_meta( $uid, 'p_firstName'))
+    {
+      add_user_meta( $uid, 'p_firstName',  $firstName);
+      add_user_meta( $uid, 'p_lastName',  $lastName);
+      add_user_meta( $uid, 'HCPC',  $HCPC);
+      add_user_meta( $uid, 'CSP',  $CSP);
+      add_user_meta( $uid, 'p_specialization',  $specialization);
+      add_user_meta( $uid, 'p_description',  $description);
+
+    $data['status'] = 'OK';
+    $data['message'] = 'Practitioner details  added successfully!';
+    $response = new WP_REST_Response($data, 201);
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Practitioner details  exists in this account!';
+    $response = new WP_REST_Response($data, 400);
+}
+}
+else
+{
+    $data['status'] = 'Failed';
+    $data['message'] = 'Authentication error';
+    $response = new WP_REST_Response($data, 400);
+}
+    return  $response;
+}
+    
+//------------Add practitioner-details end-------------//
--- readme.md
+++ readme.md
@@ -1,7 +1,8 @@
+#API 1
 
-Login : Method = post
+#Login : Method = get
 
-http://localhost/api/wp-json/api/v1/authn
+  /wp-json/api/v1/login
 
 {
     email: "ragini@ateamsoftsolutions.com",
@@ -9,11 +10,14 @@ http://localhost/api/wp-json/api/v1/authn
 }
 
 ------------------------------------------
+#API 2
 
-Register : Method = post
+#Sign in : Add user details
+
+#Register : Method = post
 
 
-http://localhost/api/wp-json/api/v1/signin
+ /wp-json/api/v1/signup
 
 {
   "title": "Ms",
@@ -26,16 +30,22 @@ http://localhost/api/wp-json/api/v1/signin
   "toc_accept": "1",
   "role": "practitioner",
   "sec_key": "secKey",
-  "btn_submit": true
+  "btn_submit": true,
+  "domain_flag" : 1
 }
 
 ----------------------------------------
-INSERT INTO `physiob1_postmeta` (`meta_id`, `post_id`, `meta_key`, `meta_value`) VALUES
+#API 3
+
+
+#Sign in : Add clinic details ; Method : post
+
+/wp-json/api/v1/signup/2
 
 
 {
   "provideruid": "00u6y27tva21PD61z4x7",
-  uid": 3432,
+  "uid": 3432,
   "listings": [
     {
       "title": "Health Sure",
@@ -59,3 +69,92 @@ INSERT INTO `physiob1_postmeta` (`meta_id`, `post_id`, `meta_key`, `meta_value`)
 }
 
 ---------------------------------------
+
+#API 4
+
+#Sign in : Add payment details ; Method = post
+
+ /wp-json/api/v1/signup/3
+
+{
+  "provideruid": "00u6y27tva21PD61z4x7",
+  "uid": 3432,
+  "paymentData": {
+    "creditor": "ABC",
+    "mandate": "XYZ",
+    "customer": "123",
+    "customer_bank_account": "1234567890"
+  }
+}
+
+
+
+---------------------------------------------
+
+#API 5
+
+#Adding clinic timings ; method = post
+
+ /wp-json/api/v1/signup/clinic-details
+
+
+{
+  "provideruid": "00u6y27tva21PD61z4x7",
+  "uid": 3432,
+  "initialAssessment": 10,
+  "followUpTreatment":20,
+  "openingTimes":[
+    {
+      "weekDays":[
+        {
+          "start":"10:00 AM",
+          "end":"1:00 PM"
+        },{
+          "start":"2:00 AM",
+          "end":"5:00 PM"
+        }
+      ],
+      "saturday":[
+        {
+          "start":"10:00 AM",
+          "end":"1:00 PM"
+        },{
+          "start":"2:00 AM",
+          "end":"3:00 PM"
+        }
+      ]
+    }
+   ],
+  "parkingMode":"none",
+  "paymentMode":"debit",
+  "insurancesAccepted":[
+    "Bupa",
+    "Aviva"
+  ],
+  "clinicDescription":"Provides customer friendly enviroment",
+  "logo":"2345jhjgsyufd5s67ahdjkakskl"
+}
+
+
+---------------------------------------------------
+
+#API 6
+
+#Adding physiotherapist : metho = post
+
+  /wp-json/api/v1/signup/practitioner-details
+
+{
+  "provideruid": "00u6y27tva21PD61z4x7",
+  "uid": 3432,
+  "firstName": "John",
+  "lastName": "Smith",
+  "profileImage": "5768hgtydufghj43fgjuhgjk",
+  "HCPC": "acde1234",
+  "CSP": "xyz123456",
+  "specialization": [
+    "Musculoskeletal Physiotherapy",
+    "Sports Injury Physiotherapy"
+  ],
+  "description": "2+ years of experience in Multispeciality Hospital"
+}
